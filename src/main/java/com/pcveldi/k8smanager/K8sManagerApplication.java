5489c4cb0271807cb84c4829da19b43da5217b8a
Pc Veldi <65594452+PcVeldi22@users.noreply.github.com>
2026-07-06 12:31:21 -0400

    Create K8sManagerApplication for Spring Boot app

---
+++ src/main/java/com/pcveldi/k8smanager/K8sManagerApplication.java
@@ -0,0 +1,12 @@
+package com.pcveldi.k8smanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sManagerApplication.class, args);
+    }
+}
